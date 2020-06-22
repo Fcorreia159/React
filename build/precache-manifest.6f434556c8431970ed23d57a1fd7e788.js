27bcf439d2058e397749947ac40c1e74bd31be30
Filipe Correia <correia481@gmail.com>
2020-06-22 19:34:13 -0300

    subindo aplicacao para heroku

---
+++ build/precache-manifest.6f434556c8431970ed23d57a1fd7e788.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33a1d403a6e0a6449457588d4182a9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a9b7a43886b2bc69c86",
+    "url": "/static/css/main.19e8cef9.chunk.css"
+  },
+  {
+    "revision": "c4219ad1722594879679",
+    "url": "/static/js/2.bef5bd9f.chunk.js"
+  },
+  {
+    "revision": "8cb75899ff78afb9456a14179577490a",
+    "url": "/static/js/2.bef5bd9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a9b7a43886b2bc69c86",
+    "url": "/static/js/main.9e73063c.chunk.js"
+  },
+  {
+    "revision": "474613d14f9d13a5dd34",
+    "url": "/static/js/runtime-main.1b6aa82a.js"
+  },
+  {
+    "revision": "09f06cb542c659a4a83d320fe266d229",
+    "url": "/static/media/unnamed.09f06cb5.png"
+  }
+]);
