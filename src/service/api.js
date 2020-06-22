e6cb4e9d24977946e9e7438c6bb9338d47f812c9
Filipe Correia <correia481@gmail.com>
2020-06-22 20:21:22 -0300

    erros tecnicos

--- src/service/api.js
+++ src/service/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'https://cors-anywhere.herokuapp.com/https://aw-doaai-api.herokuapp.com/',
-  baseURL: 'https://aw-doaai-api.herokuapp.com/',
-  //baseURL: 'https://cors-anywhere.herokuapp.com/https://aw-doaai-api.herokuapp.com/',
+  //baseURL: 'https://aw-doaai-api.herokuapp.com/',
+  baseURL: 'https://cors-anywhere.herokuapp.com/https://aw-doaai-api.herokuapp.com/',
 });
 
 export default api;
